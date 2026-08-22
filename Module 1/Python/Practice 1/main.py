361b07850ce1d293b0463c2101502d933184b80c
Abdul-Samad-Feroz <abdulsamadferoz786@gmail.com>
2026-08-22 18:22:34 +0500

    Practice 1

---
+++ Module 1/Python/Practice 1/main.py
@@ -0,0 +1,16 @@
+print("Hello word")
+
+# This is a comment
+
+print("Hello World")  # This is a comment
+
+firstVariable = 10
+print(firstVariable)
+
+# We can create multiple variables in a line
+
+a, b, c = "Apple", "Banana", "Cherry"
+
+print(a)
+print(b)
+print(c)
