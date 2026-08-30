e9e87debe59c345d21e31045d4ba148e39057017
Abdul-Samad-Feroz <abdulsamadferoz786@gmail.com>
2026-08-30 23:30:47 +0500

    For Loop

---
+++ Module 1/Python/Class Practice 3/main.py
@@ -0,0 +1,57 @@
+print("Anas")
+print("Anas")
+print("Anas")
+print("Anas")
+print("Anas")
+
+
+# For Loop
+for i in range(5):
+    print("A.Samad")
+
+print(range(5))
+
+for i in range(7):
+    print(i)
+
+for i in range(10):
+    print(i)
+
+for i in range(11):
+    print("Python is fun")
+
+# range(start, stop, step)
+for i in range(1, 9, 2):
+    print(i)
+
+for i in range(10, -1, -1):
+    print(i)
+
+
+for i in range(2, 21, 2):
+    print(i)
+
+for i in range(1, 20, 2):
+    print(i)
+
+for i in range(5, 51, 5):
+    print(i)
+
+
+# Loop + Arithmetic
+
+table = int(input("Enter Table number: "))
+
+for i in range(1, 11):
+    print(table, "X", i, "=", table * i)
+
+name = input("Enter a Name you want to print: ")
+times = int(input("How many times do you want to print this name? "))
+
+for i in range(times):
+    print("#", i + 1, name)
+
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print(i)
