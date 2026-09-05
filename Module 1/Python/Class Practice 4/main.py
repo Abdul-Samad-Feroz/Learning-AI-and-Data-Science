b4b4dcf25257822b1deddc15f637876f50bdbdd0
Abdul-Samad-Feroz <abdulsamadferoz786@gmail.com>
2026-09-05 20:50:16 +0500

    For Loop & Accumulator

---
+++ Module 1/Python/Class Practice 4/main.py
@@ -0,0 +1,62 @@
+for i in range(10, 0, -2):
+    print(i)
+
+for i in range(1, 6):
+    print(i * 10)
+
+
+total = 0
+for i in range(6):
+    total = total + i  # Accumulator
+print(total)
+
+total = 0
+for i in range(11):
+    total = total + i
+print(total)
+
+total = 0
+n = int(input("Enter the Num Range: "))
+for i in range(1, n + 1):
+    total = total + i
+    print(total)
+
+totalNum = 0
+for i in range(1, 101):
+    if i % 2 == 0:
+        totalNum = totalNum + i
+print("Sum of even numbers from 1 to 100 is:", totalNum)
+
+totalNum = 0
+for i in range(1, 101):
+    if i % 2 != 0:
+        totalNum = totalNum + i
+print("Sum of odd numbers from 1 to 100 is:", totalNum)
+
+count = 0
+for i in range(1, 21):
+    if i % 2 == 0:
+        count = count + 1
+print("Count of even numbers between 1 to 20 is:", count)
+
+count = 0
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        count += 1
+print(count)
+
+student = int(input("Enter number of Students:"))
+total = 0
+passed = 0
+failed = 0
+for i in range(student):
+    marks = float(input("Enter Marks:"))
+    total = total + marks
+    if marks >= 50:
+        passed = passed + 1
+    else:
+        failed = failed = failed + 1
+print("Total:", total)
+print("Average:", total / student)
+print("Passed Students:", passed)
+print("Failed Students:", failed)
